f45973ddebd1064f96d71515cf063646ce6740af
binzhang <bin.zhang.u5f6c@outlook.com>
2020-01-06 21:14:28 +1100

    Jan.6th.2020.6th

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -4,6 +4,7 @@ from math import log
 from yaml import safe_load
 import torch
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 with open('config.yml', 'r') as f:
     conf = safe_load(f)
 p = conf['p']
@@ -15,10 +16,10 @@ def preprocess(x, loss):
     col2 = x.sign()
     col2[x.abs() < exp(-p)] = x[x.abs() < exp(-p)] * exp(p)
     if abs(loss) >= exp(-p):
-        col3 = torch.ones(x.size()) * (log(abs(loss))/p)
-        col4 = torch.ones(x.size()) * (loss/abs(loss))
+        col3 = torch.ones(x.size(), device=device) * (log(abs(loss))/p)
+        col4 = torch.ones(x.size(), device=device) * (loss/abs(loss))
     else:
-        col3 = torch.ones(x.size()) * (-1)
-        col4 = torch.ones(x.size()) * (exp(p)*loss)
+        col3 = torch.ones(x.size(), device=device) * (-1)
+        col4 = torch.ones(x.size(), device=device) * (exp(p)*loss)
     re = torch.cat([col1, col2, col3, col4], 1)
     return re.reshape(-1, 1, 4)
