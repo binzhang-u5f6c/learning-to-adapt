feaa6c3bcfd161a9ecc53aceb14632aacecb7912
binzhang <bin.zhang.u5f6c@outlook.com>
2020-01-07 11:17:02 +1100

    Jan.7th.2020.5th

--- utils/load.py
+++ utils/load.py
@@ -33,11 +33,11 @@ class DatasetFoo(Dataset):
         self.y[cols[0]] = ybar.replace(replace_map)
 
         if train:
-            self.x = self.x[:split_point, :]
-            self.y = self.y[:split_point, :]
+            self.x = self.x.iloc[:split_point, :]
+            self.y = self.y.iloc[:split_point, :]
         else:
-            self.x = self.x[split_point:, :]
-            self.y = self.y[split_point:, :]
+            self.x = self.x.iloc[split_point:, :]
+            self.y = self.y.iloc[split_point:, :]
 
     def __len__(self):
         return len(self.x)
