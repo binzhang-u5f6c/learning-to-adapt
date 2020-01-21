38c9ec597c47c9c0790ceca07b5cc02d116bc8f3
binzhang <bin.zhang.u5f6c@outlook.com>
2020-01-22 03:30:05 +1100

    Jan.22th.2020.6th

--- train/getinitbaselearner.py
+++ train/getinitbaselearner.py
@@ -2,36 +2,100 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-from model.baselearner import BaseLearner
-from utils.load import get_dataloader
+from model.baselearner import BaseLearner1
+from data.getdataloader import get_hyperplane1a
+from data.getdataloader import get_hyperplane2a
+from data.getdataloader import get_sea1a
+from data.getdataloader import get_sea2a
+
+batch_size = 100
+epoch = 100
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+dl = get_hyperplane1a(batch_size)
+model = BaseLearner1(10, 5, 2)
+model.to(device)
+model.double()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+for e in range(epoch):
+    for x, y in dl:
+        x = x.to(device)
+        x = x.double()
+        y = y.to(device)
+        y = y.long()
+        y = y.view(-1)
 
-def get_init_baselearner(filename, hidden_size, batch_size,
-                         training_size, epoch, lr):
-    dataloader, input_size, output_size = get_dataloader(filename,
-                                                         batch_size,
-                                                         training_size)
+        optimizer.zero_grad()
+        ybar = model(x)
+        loss = F.nll_loss(ybar, y)
+        loss.backward()
+        optimizer.step()
 
-    model = BaseLearner(input_size, hidden_size, output_size)
-    model.to(device)
-    model.double()
-    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+torch.save(model.state_dict(), 'model/hp1.pt')
 
-    for e in range(epoch):
-        for batch_idx, (batch_x, batch_y) in enumerate(dataloader):
-            batch_x = batch_x.to(device)
-            batch_x = batch_x.double()
-            batch_y = batch_y.to(device)
-            batch_y = batch_y.long()
-            batch_y = batch_y.view(-1)
 
-            optimizer.zero_grad()
+dl = get_hyperplane2a(batch_size)
+model = BaseLearner1(10, 5, 2)
+model.to(device)
+model.double()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+for e in range(epoch):
+    for x, y in dl:
+        x = x.to(device)
+        x = x.double()
+        y = y.to(device)
+        y = y.long()
+        y = y.view(-1)
 
-            ybar = model(batch_x)
-            loss = F.nll_loss(ybar, batch_y)
-            loss.backward()
-            optimizer.step()
+        optimizer.zero_grad()
+        ybar = model(x)
+        loss = F.nll_loss(ybar, y)
+        loss.backward()
+        optimizer.step()
 
-    torch.save(model.state_dict(), filename[5:]+'.pt')
+torch.save(model.state_dict(), 'model/hp2.pt')
+
+
+dl = get_sea1a(batch_size)
+model = BaseLearner1(3, 5, 2)
+model.to(device)
+model.double()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+for e in range(epoch):
+    for x, y in dl:
+        x = x.to(device)
+        x = x.double()
+        y = y.to(device)
+        y = y.long()
+        y = y.view(-1)
+
+        optimizer.zero_grad()
+        ybar = model(x)
+        loss = F.nll_loss(ybar, y)
+        loss.backward()
+        optimizer.step()
+
+torch.save(model.state_dict(), 'model/sea1.pt')
+
+
+dl = get_sea2a(batch_size)
+model = BaseLearner1(3, 5, 2)
+model.to(device)
+model.double()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+for e in range(epoch):
+    for x, y in dl:
+        x = x.to(device)
+        x = x.double()
+        y = y.to(device)
+        y = y.long()
+        y = y.view(-1)
+
+        optimizer.zero_grad()
+        ybar = model(x)
+        loss = F.nll_loss(ybar, y)
+        loss.backward()
+        optimizer.step()
+
+torch.save(model.state_dict(), 'model/sea2.pt')
