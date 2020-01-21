38106616dcd6995a11cc2d9c0a8bb066121df946
binzhang <bin.zhang.u5f6c@outlook.com>
2020-01-22 05:22:59 +1100

    Jan.22th.2020.9th

--- train/getmetalearner1.py
+++ train/getmetalearner1.py
@@ -2,83 +2,120 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-from model.baselearner import BaseLearner
+from model.baselearner import BaseLearner1
 from model.metalearner import MetaLearner
-from utils.load import get_dataloader
+from data.getdataloader import get_hyperplane1b
+from data.getdataloader import get_sea1b
 from utils.getgrad import get_grad
 from utils.preprocess import preprocess
 
-# hyperparemeters
+batch_size1 = 2000
+batch_size2 = 100
+epoch = 10
+T = 10
+p = 10
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-files = ['data/airlines.arff',
-         'data/covtype.arff',
-         'data/kddcup99.arff',
-         'data/pokerhand.arff']
 
+metalearner = MetaLearner(5)
+metalearner.to(device)
+metalearner.double()
+optimizer = optim.SGD(metalearner.parameters(), lr=0.01, momentum=0.9)
 
-def get_metalearner1(filename, batch_size1, batch_size2, hidden_size,
-                     m_hidden_size, training_size, epoch, lr, T, p):
-    metalearner = MetaLearner(m_hidden_size)
-    metalearner.to(device)
-    metalearner.double()
-    optimizer = optim.SGD(metalearner.parameters(), lr=lr, momentum=0.9)
+dl1 = get_hyperplane1b(batch_size1)
+dl2 = get_sea1b(batch_size1)
 
-    for e in range(epoch):
-        for f in files:
-            if f == filename:
-                continue
-            dataloader, input_size, output_size = \
-                get_dataloader(f, batch_size1, training_size, False)
+for e in range(epoch):
+    model = BaseLearner1(10, 5, 2)
+    model.load_state_dict(torch.load('data/hp1.pt'))
+    model.double()
+    model.to(device)
 
-            model = BaseLearner(input_size, hidden_size, output_size)
-            model.load_state_dict(torch.load(f[5:]+'.pt'))
-            model.double()
-            model.to(device)
+    model_cp = BaseLearner1(10, 5, 2)
+    model_cp.double()
+    model_cp.to(device)
 
-            model_cp = BaseLearner(input_size, hidden_size, output_size)
-            model_cp.double()
-            model_cp.to(device)
+    for x, y in dl1:
+        x = x.to(device)
+        x = x.double()
+        x = x.view(-1, batch_size2, 10)
+        y = y.to(device)
+        y = y.long()
+        y = y.view(-1, batch_size2)
 
-            for batch_x, batch_y in dataloader:
-                batch_x = batch_x.to(device)
-                batch_x = batch_x.double()
-                batch_x = batch_x.view(-1, batch_size2, input_size)
-                batch_y = batch_y.to(device)
-                batch_y = batch_y.long()
-                batch_y = batch_y.view(-1, batch_size2)
+        for j in range(x.size(0)):
+            optimizer.zero_grad()
+            for t in range(T):
+                model_cp.load_state_dict(model.state_dict())
+                grad, loss = get_grad(model_cp, x[j][:50], y[j][:50])
+                hc = [None for i in grad]
+                for n, para in enumerate(model.parameters()):
+                    meta_input = preprocess(grad[n], loss, p)
+                    meta_input.to(device)
+                    if hc[n] is None:
+                        hc[n] = (torch.zeros(2, meta_input.size(0),
+                                             5, device=device,
+                                             dtype=torch.float64),
+                                 torch.zeros(2, meta_input.size(0),
+                                             5, device=device,
+                                             dtype=torch.float64))
+                    meta_output, hc[n] = metalearner(meta_input, hc[n])
 
-                for j in range(batch_x.size(0)-1):
-                    optimizer.zero_grad()
-                    for t in range(T):
-                        model_cp.load_state_dict(model.state_dict())
-                        grad, loss = get_grad(model_cp, batch_x[j], batch_y[j])
-                        hc = [None for i in grad]
-                        for n, para in enumerate(model.parameters()):
-                            meta_input = preprocess(grad[n], loss, p)
-                            meta_input.to(device)
-                            if hc[n] is None:
-                                hc[n] = (torch.zeros(2, meta_input.size(0),
-                                                     m_hidden_size,
-                                                     device=device,
-                                                     dtype=torch.float64),
-                                         torch.zeros(2, meta_input.size(0),
-                                                     m_hidden_size,
-                                                     device=device,
-                                                     dtype=torch.float64))
-                            meta_output, hc[n] = \
-                                metalearner(meta_input, hc[n])
+                    meta_output = meta_output.view(para.data.size())
+                    para.data.sub_(meta_output)
+            l2 = 0
+            for para in model.parameters():
+                l2 += (para**2).sum()
+            ybar = model(x[j][50:])
+            t_loss = F.nll_loss(ybar, y[j][50:]) + l2
+            t_loss.backward()
+            torch.nn.utils.clip_grad_value_(metalearner.parameters(), 10)
+            optimizer.step()
 
-                            meta_output = meta_output.view(para.data.size())
-                            para.data.sub_(meta_output)
+    model = BaseLearner1(3, 5, 2)
+    model.load_state_dict(torch.load('data/sea1.pt'))
+    model.double()
+    model.to(device)
 
-                    l2 = 0
-                    for para in model.parameters():
-                        l2 += (para**2).sum()
-                    ybar = model(batch_x[j+1])
-                    t_loss = F.nll_loss(ybar, batch_y[j+1]) + l2
-                    t_loss.backward()
-                    torch.nn.utils.clip_grad_value_(metalearner.parameters(),
-                                                    10)
-                    optimizer.step()
+    model_cp = BaseLearner1(3, 5, 2)
+    model_cp.double()
+    model_cp.to(device)
 
-    torch.save(metalearner.state_dict(), 'meta1.'+filename[5:]+'.pt')
+    for x, y in dl2:
+        x = x.to(device)
+        x = x.double()
+        x = x.view(-1, batch_size2, 10)
+        y = y.to(device)
+        y = y.long()
+        y = y.view(-1, batch_size2)
+
+        for j in range(x.size(0)):
+            optimizer.zero_grad()
+            for t in range(T):
+                model_cp.load_state_dict(model.state_dict())
+                grad, loss = get_grad(model_cp, x[j][:50], y[j][:50])
+                hc = [None for i in grad]
+                for n, para in enumerate(model.parameters()):
+                    meta_input = preprocess(grad[n], loss, p)
+                    meta_input.to(device)
+                    if hc[n] is None:
+                        hc[n] = (torch.zeros(2, meta_input.size(0),
+                                             5, device=device,
+                                             dtype=torch.float64),
+                                 torch.zeros(2, meta_input.size(0),
+                                             5, device=device,
+                                             dtype=torch.float64))
+                    meta_output, hc[n] = metalearner(meta_input, hc[n])
+
+                    meta_output = meta_output.view(para.data.size())
+                    para.data.sub_(meta_output)
+            l2 = 0
+            for para in model.parameters():
+                l2 += (para**2).sum()
+            ybar = model(x[j][50:])
+            t_loss = F.nll_loss(ybar, y[j][50:]) + l2
+            t_loss.backward()
+            torch.nn.utils.clip_grad_value_(metalearner.parameters(), 10)
+            optimizer.step()
+
+torch.save(metalearner.state_dict(), 'meta.pt')
