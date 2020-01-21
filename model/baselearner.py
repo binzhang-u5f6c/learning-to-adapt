98b91ab9fbde463be667be22edbc2e79fd5da885
binzhang <bin.zhang.u5f6c@outlook.com>
2020-01-21 22:44:36 +1100

    Jan.21th.2020.6th

--- model/baselearner.py
+++ model/baselearner.py
@@ -6,7 +6,11 @@ class BaseLearner(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(BaseLearner, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
+        nn.init.kaiming_normal_(self.fc1.weight)
+        nn.init.kaiming_normal_(self.fc1.bias)
         self.fc2 = nn.Linear(hidden_size, output_size)
+        nn.init.kaiming_normal_(self.fc2.weight)
+        nn.init.kaiming_normal_(self.fc2.bias)
 
     def forward(self, x):
         x = self.fc1(x)
