17ef8597810a1507d7d2f16f7ddf90884d1a1481
binzhang <bin.zhang.u5f6c@outlook.com>
2020-01-22 11:39:33 +1100

    Jan.22th.2020.13th

--- evaluate/lftee1.py
+++ evaluate/lftee1.py
@@ -1,69 +1,111 @@
 import torch
 
-from model.baselearner import BaseLearner
+from model.baselearner import BaseLearner1
 from model.metalearner import MetaLearner
-from utils.load import get_dataloader
+from data.getdataloader import get_hyperplane2b
+from data.getdataloader import get_sea2b
 from utils.getgrad import get_grad
 from utils.preprocess import preprocess
 
+batch_size1 = 2000
+batch_size2 = 100
+T = 10
+p = 10
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-def lftee1(filename, batch_size1, batch_size2, hidden_size,
-           meta_hidden_size, training_size, T, p):
-    dataloader, input_size, output_size = get_dataloader(filename, batch_size1,
-                                                         training_size, False)
+def lftee1():
+    metalearner = MetaLearner(5)
+    metalearner.load_state_dict(torch.load('model/meta.pt'))
+    metalearner.to(device)
+    metalearner.double()
 
-    model = BaseLearner(input_size, hidden_size, output_size)
-    model.load_state_dict(torch.load(filename[5:]+'.pt'))
+    dl = get_hyperplane2b(batch_size1)
+    model = BaseLearner1(10, 5, 2)
+    model.load_state_dict(torch.load('data/hp2.pt'))
     model.double()
     model.to(device)
 
-    model_cp = BaseLearner(input_size, hidden_size, output_size)
+    model_cp = BaseLearner1(10, 5, 2)
     model_cp.double()
     model_cp.to(device)
 
-    metalearner = MetaLearner(meta_hidden_size)
-    metalearner.load_state_dict(torch.load('meta1.'+filename[5:]+'.pt'))
-    metalearner.double()
-    metalearner.to(device)
+    total, corr = 0, 0
+    for x, y in dl:
+        x = x.to(device)
+        x = x.double()
+        x = x.view(-1, batch_size2, 10)
+        y = y.to(device)
+        y = y.long()
+        y = y.view(-1, batch_size2)
 
-    total = 0
-    correct = 0
-    for batch_x, batch_y in dataloader:
-        batch_x = batch_x.to(device)
-        batch_x = batch_x.double()
-        batch_x = batch_x.view(-1, batch_size2, input_size)
-        batch_y = batch_y.to(device)
-        batch_y = batch_y.long()
-        batch_y = batch_y.view(-1, batch_size2)
-
-        for j in range(batch_x.size(0)):
-            ybar = model(batch_x[j])
+        for j in range(x.size(0)):
+            ybar = model(x[j])
             ybar = ybar.max(1)[1]
             ybar = ybar.view(-1)
             total += batch_size2
-            correct += (ybar == batch_y[j]).sum().item()
+            corr += (ybar == y[j]).sum().item()
             for t in range(T):
                 model_cp.load_state_dict(model.state_dict())
-                grad, loss = get_grad(model_cp, batch_x[j], batch_y[j])
+                grad, loss = get_grad(model_cp, x[j], y[j])
                 hc = [None for i in grad]
                 for n, para in enumerate(model.parameters()):
                     meta_input = preprocess(grad[n], loss, p)
                     meta_input.to(device)
                     if hc[n] is None:
                         hc[n] = (torch.zeros(2, meta_input.size(0),
-                                             meta_hidden_size,
-                                             device=device,
+                                             5, device=device,
                                              dtype=torch.float64),
                                  torch.zeros(2, meta_input.size(0),
-                                             meta_hidden_size,
-                                             device=device,
+                                             5, device=device,
                                              dtype=torch.float64))
-                    meta_output, hc[n] = \
-                        metalearner(meta_input, hc[n])
+                    meta_output, hc[n] = metalearner(meta_input, hc[n])
 
                     meta_output = meta_output.view(para.data.size())
                     para.data.sub_(meta_output)
+    print('hyperplane acc:{:.2f}%'.format(corr/total*100))
 
-    return correct, total
+    dl = get_sea2b(batch_size1)
+    model = BaseLearner1(3, 5, 2)
+    model.load_state_dict(torch.load('data/sea2.pt'))
+    model.double()
+    model.to(device)
+
+    model_cp = BaseLearner1(3, 5, 2)
+    model_cp.double()
+    model_cp.to(device)
+
+    total, corr = 0, 0
+    for x, y in dl:
+        x = x.to(device)
+        x = x.double()
+        x = x.view(-1, batch_size2, 3)
+        y = y.to(device)
+        y = y.long()
+        y = y.view(-1, batch_size2)
+
+        for j in range(x.size(0)):
+            ybar = model(x[j])
+            ybar = ybar.max(1)[1]
+            ybar = ybar.view(-1)
+            total += batch_size2
+            corr += (ybar == y[j]).sum().item()
+            for t in range(T):
+                model_cp.load_state_dict(model.state_dict())
+                grad, loss = get_grad(model_cp, x[j], y[j])
+                hc = [None for i in grad]
+                for n, para in enumerate(model.parameters()):
+                    meta_input = preprocess(grad[n], loss, p)
+                    meta_input.to(device)
+                    if hc[n] is None:
+                        hc[n] = (torch.zeros(2, meta_input.size(0),
+                                             5, device=device,
+                                             dtype=torch.float64),
+                                 torch.zeros(2, meta_input.size(0),
+                                             5, device=device,
+                                             dtype=torch.float64))
+                    meta_output, hc[n] = metalearner(meta_input, hc[n])
+
+                    meta_output = meta_output.view(para.data.size())
+                    para.data.sub_(meta_output)
+    print('sea acc:{:.2f}%'.format(corr/total*100))
